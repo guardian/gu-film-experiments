7a56788035cbb86d8b127fd49a9c4da9b35ad508
Lindsey Dew <lindsey.dew@guardian.co.uk>
2013-11-26 17:12:52 +0000

    add endpoints for calling the star ratings

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import jinja2
 import os
 import json
 import logging
+import handlers
 from urllib import quote, urlencode
+
 from google.appengine.api import urlfetch
 
 jinja_environment = jinja2.Environment(
@@ -17,5 +19,7 @@ class MainPage(webapp2.RequestHandler):
 
 		self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+app = webapp2.WSGIApplication([('/', MainPage),
+                                webapp2.Route(r'/reviews/stars/<max_or_min>/<star_value>', handler = handlers.StarRated),
+                                webapp2.Route(r'/reviews/stars/<star_value>', handler = handlers.StarRated)],
                               debug=True)
